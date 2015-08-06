26bebef01ef4e2072c75bb7f5d0d6ae78f0802bc
herry <herryhou@gmail.com.tw>
2015-08-06 18:10:28 +0800

    elk.sh now works!

--- elk.sh
+++ elk.sh
@@ -6,16 +6,15 @@ docker run -d \
  --name elk_elasticsearch1 \
  -p 9200:9200 \
  -p 9300:9300 \
- -e HOST_NAME=$hostname \
- -e HOST_IP=$host_ip \
  elk_elasticsearch:latest \
  elasticsearch \
- -Des.node.name="$HOST_NAME" \
- -Des.network.publish_host=$HOST_IP \
+ -Des.node.name=$(hostname) \
+ -Des.network.publish_host=$host_ip \
  -Des.network.bind_host=0.0.0.0 \
+ -Des.multicast.enabled=false \
  -Des.discovery.zen.ping.multicast.enabled=false \
  -Des.discovery.zen.ping.unicast.hosts=192.168.10.171:9300,192.168.10.172:9300,192.168.10.173:9300 \
- -Des.cluster.name="Docker_ES" && \
+ -Des.cluster.name="DockerES" && \
 docker run -d \
  --name elk_kibana1 \
  -p 8080:8080 \
