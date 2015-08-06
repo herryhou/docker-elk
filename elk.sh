a2d58a6cf2527cc48680a222f2facd16d58a72dc
herry <herryhou@gmail.com.tw>
2015-08-06 16:58:08 +0800

    docker-compose dose not support "environment variables injected" yet, so use this elk.sh instead.

---
+++ elk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+host_ip=$(/sbin/ip route|awk '/eth0.*link/ { print $9 }')
+
+docker run -d \
+ --name elk_elasticsearch1 \
+ -p 9200:9200 \
+ -p 9300:9300 \
+ -e HOST_NAME=$hostname \
+ -e HOST_IP=$host_ip \
+ elk_elasticsearch:latest \
+ elasticsearch \
+ -Des.node.name="$HOST_NAME" \
+ -Des.network.publish_host=$HOST_IP \
+ -Des.network.bind_host=0.0.0.0 \
+ -Des.cluster.name="Docker_ES" && \
+docker run -d \
+ --name elk_kibana1 \
+ -p 8080:8080 \
+ --link elk_elasticsearch1 \
+ elk_kibana:latest \
+ ./run.sh
